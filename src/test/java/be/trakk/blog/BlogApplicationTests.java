9c4775daa72ac6cde36fa0947c3c29abd7eade45
Gregceuleers <34240971+Gregceuleers@users.noreply.github.com>
2020-09-19 14:37:02 +0200

    Upload solution

---
+++ src/test/java/be/trakk/blog/BlogApplicationTests.java
@@ -0,0 +1,13 @@
+package be.trakk.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
